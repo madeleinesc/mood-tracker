e3c29c5766d48cc2a0dc83f14fbee70cf2976eeb
Madeleinesc <madeleine.s.cosway@gmail.com>
2022-02-20 23:45:51 +1030

    modified welcome.js and css for mood colors

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -9,41 +9,49 @@ const Welcome = () => {
         <body>
             <div className="container">
                 <h2 className="display-5 text-center text-dark user-title">Hi <span> {Auth.getProfile().data.username}</span>, how are you feeling today?</h2>
-                <p className="lead text-muted text-center">{moment().format("Do MMMM YYYY")}
-
-
-
-
-
-10:40
- </p>
-
+                <p className="lead text-muted text-center">{moment().format("Do MMMM YYYY")}</p>
 
                 <p className="lead text-center pt-3">Select the colour that best represents your mood:</p>
-                <div className="container radioContainer ">
-                    <div className="row">
-                        <div className="col-4 radioBlock happy">Happy</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-4 radioBlock joyful">Joyful</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-4 radioBlock Neutral">Neutral</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-4 radioBlock Angry">Angry</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-4 radioBlock Sad">Sad</div>
-                    </div>
-                    <div className="row">
-                        <div className="col-4 radioBlock Depressed">Depressed</div>
+                <div className="container radioContainer " >
+                    <div className="row" >
+                        <div className="col-6 col-sm-4 ">
+                            <div className='radioBlock happy'>
+                                Happy
+                            </div>
+                        </div>
+                        <div className="col-6 col-sm-4 ">
+                            <div className='radioBlock joyful'>
+                                Joyful
+                            </div>
+                        </div>
+                        <div className="col-6 col-sm-4 ">
+                            <div className='radioBlock neutral'>
+                                Neutral
+                            </div>
+                        </div>
+                        <div className="col-6 col-sm-4 ">
+                            <div className='radioBlock angry'>
+                                angry
+                            </div>
+                        </div>
+                        <div className="col-6 col-sm-4 ">
+                            <div className='radioBlock sad'>
+                                Sad
+                            </div>
+                        </div>
+                        <div className="col-6 col-sm-4 ">
+                            <div className='radioBlock depressed'>
+                                Depressed
+                            </div>
+                        </div>
                     </div>
 
-                
 
                 </div>
 
+                </div>
+
+                
                 <div class="text-center sign-btn">
             <button type="button" class="btn btn-dark">Submit</button> 
           </div>
@@ -58,7 +66,7 @@ const Welcome = () => {
 
 
 
-            </div>
+            
 
 
 
