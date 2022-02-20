2cc2cad06f07dde442609075ce8196f39448b5fe
Madeleinesc <madeleine.s.cosway@gmail.com>
2022-02-20 21:36:32 +1030

    added welcome.js

---
+++ client/src/pages/Welcome.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Welcome = () => {
+
+  return (
+    <body>
+        <div class="container">
+          <h2 class="display-5 text-center text-dark user-title">Hi User, how are you feeling today?</h2>
+          <p class="lead text-muted text-center">24th Feb 2022</p>
+      </div>
+    </body>
+  );
+};
+
+export default Welcome;
