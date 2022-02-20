0db6554ffcf0b991293b646b02fbe49c88d59b90
Madeleinesc <madeleine.s.cosway@gmail.com>
2022-02-21 06:26:04 +1030

    created moods page

---
+++ client/src/pages/Moods.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Auth from '../utils/auth';
+
+const Moods = () => {
+    return (
+        <body>
+            <div className="container">
+            <h2 className="display-5 text-center text-dark user-title">A year in moods.</h2>
+            </div>
+        </body>
+    )
+}
+
+export default Moods;
