da5d0071c10bd4ef4565276fdde2af94831ef5d0
Madeleinesc <madeleine.s.cosway@gmail.com>
2022-02-20 21:42:56 +1030

    modified .env

--- client/src/pages/Welcome.js
+++ client/src/pages/Welcome.js
@@ -5,7 +5,7 @@ const Welcome = () => {
   return (
     <body>
         <div class="container">
-          <h2 class="display-5 text-center text-dark user-title">Hi User, how are you feeling today?</h2>
+          <h2 class="display-5 text-center text-dark user-title">Hi <span>Hey there, {Auth.getProfile().data.username}!</span>, how are you feeling today?</h2>
           <p class="lead text-muted text-center">24th Feb 2022</p>
       </div>
     </body>
